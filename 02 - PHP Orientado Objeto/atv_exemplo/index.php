755bedbeb20a81353e16e6c1709e3090154642b1
HiFuruya <h.furuya@hotmail.com>
2022-06-04 20:20:18 -0300

    Fazendo o exemplo rodar no meu notebook

---
+++ 02 - PHP Orientado Objeto/atv_exemplo/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    session_start();
+
+    include_once 'global.php';
+    include_once 'controllers/RouteController.php';
+
+    // Exibição de Erros - PHP
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    // Controller Principal - Carrega Página Principal
+    RouteController::index();
+?>
