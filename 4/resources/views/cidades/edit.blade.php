537e0efa19ef37a8b234cf81ba21bd4c5e3733a8
HiFuruya <h.furuya@hotmail.com>
2022-06-24 11:21:59 -0300

    Atv5 concluida

---
+++ 4/resources/views/cidades/edit.blade.php
@@ -0,0 +1,25 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<div class="row">
+   <div class="col d-flex justify-content-begin">
+      <a href="{{route('cidades.index')}}">
+         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+         </svg>
+      </a>
+      <h1>Alterar Cadastro</h1>
+   </div>
+</div>
+<form action="{{ route('cidades.update', $dados['id']) }}" method="POST">
+   <!-- Token de Segurança -->
+   <!-- Define o método de submissão como PUT -->
+   @csrf
+   @method('PUT')
+   <label>Nome: </label> <input type='text' name='nome' value='{{$dados['nome']}}'>
+   <label>Porte: </label> <input type='text' name='porte' value='{{$dados['porte']}}'>
+   <button type="submit" class="btn btn-success">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+         <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+      </svg>
+   </button>
+</form>
